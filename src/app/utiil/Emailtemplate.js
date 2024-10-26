f09f2bebabefa2ee23ce773b2fbc0a940b6f1d90
Karn-2906 <Karangandhi486@gmail.com>
2024-10-27 01:13:41 +0530

    email template

--- src/app/utiil/Emailtemplate.js
+++ src/app/utiil/Emailtemplate.js
@@ -5,7 +5,7 @@ const recipitentmail = (name) => {
             <h2>Thank you for contacting me</h2>
             <h3>I will contact you as soon as possible</h3>
             <h3>Have a look at my Works on <a href="https://github.com/karan-2906" target="_blank" style="color: FF56F6">Github</a> <br /> Also Visit my <a href="https://portfolio-karan-gandhi.vercel.app/" style="color: FF56F6">portfolio</a> </h3>
-            <h3>You and reply to this mail to initiate a talk on topic you mentioned</h3>
+            <h3>You can reply to this mail to initiate a talk on topic you mentioned</h3>
             <br />
             <h4>Regards</h4>
             <h4>Thank you</h4>
