6975ad6091edb5a702e97e41555348f49d333e96
Karn-2906 <Karangandhi486@gmail.com>
2024-12-25 11:51:06 +0530

    SEO keywords

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,8 @@ import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export const metadata: Metadata = {
   title: "Karan Gandhi",
-  description: "Portfolio, Karan Gandhi, Portfolio Website, Karan Gandhi Portfolio, Karan2906, Karan Bhavesh Gandhi",
+  description: "This is the portfolio website build by Karan Gandhi",
+  keywords: "Portfolio, Karan Gandhi, Portfolio Website, Karan Gandhi Portfolio, Karan2906, Karan Bhavesh Gandhi, Karan Gandhi Portfolio Website, Karan, Gandhi, Karan2906 Portfolio, Karan Bhavesh Gandhi Portfolio, website, web development, web developer, front-end developer, front-end, developer, react developer, react, reactjs, nextjs, next.js, tailwindcss, tailwind css, tailwind, javascript, typescript, nodejs, node.js, express, expressjs, express.js, mongodb, back-end developer, back-end"
 };
 
 export default function RootLayout({
